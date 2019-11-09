2902858ed2de7bdf9024afddb515435a322c2093
Dustin Martinez <martinezdustin736@gmail.com>
2019-11-09 14:15:04 -0600

    update files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,20 +6,17 @@
 
 
 $( "#search-form" ).submit(function( event ) {
-    event.preventDefault();
-    const value = document.getElementById('textarea1').value
-    console.log(value);
-    $('#search-form')[0].reset();
-    //call api here 
-    var queryURL ="https://developer.nps.gov/api/v1/parks?stateCode=" + value + "&api_key=a3wuSw2Td9o5F68gcq0f7e4Q8HzlBztPwFUaHzZ5"
-  
-
-    $.ajax({
-      url: queryURL,
+  event.preventDefault();
+  const value = document.getElementById('textarea1').value
+  console.log(value);
+  $('#search-form')[0].reset();
+  let queryUrl ="https://developer.nps.gov/api/v1/parks?stateCode=" + value + "&api_key=a3wuSw2Td9o5F68gcq0f7e4Q8HzlBztPwFUaHzZ5"; 
+  $.ajax({
+      url: queryUrl,
       method: "GET"
-    }).then(function(response){
-      console.log(response);
-    })
+  }).then(function(response){
+      console.log(response)
+      console.log(response.data.fullName)
+  })
 
-
-  });
+});
