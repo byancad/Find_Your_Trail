d6a7de4bb61ad1f372b32f88884b34f33753b911
Dustin Martinez <martinezdustin736@gmail.com>
2019-11-09 13:28:37 -0600

    files

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+//trigger an autoresize on it afterwords because .val() 
+// does not automatically trigger the events we've binded to the textarea.
+
+//   $('#textarea1').val('New Text');
+//   M.textareaAutoResize($('#textarea1'));
+
+
+$( "#search-form" ).submit(function( event ) {
+    event.preventDefault();
+    const value = document.getElementById('textarea1').value
+    console.log(value);
+    $('#search-form')[0].reset();
+    //call api here 
+    var queryURL ="https://developer.nps.gov/api/v1/parks?stateCode=" + value + "&api_key=a3wuSw2Td9o5F68gcq0f7e4Q8HzlBztPwFUaHzZ5"
+  
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response){
+      console.log(response);
+    })
+
+
+  });
