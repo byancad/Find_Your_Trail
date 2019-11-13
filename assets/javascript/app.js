bce1a24a56e47be34f14eee9ebda356717e86489
Dustin Martinez <martinezdustin736@gmail.com>
2019-11-12 20:12:35 -0600

    files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+
 //trigger an autoresize on it afterwords because .val() 
 // does not automatically trigger the events we've binded to the textarea.
 
@@ -5,23 +6,33 @@
 //   M.textareaAutoResize($('#textarea1'));
 
 
-$( "#search-form" ).submit(function( event ) {
+$("#search-form").submit(function( event ) {
   event.preventDefault();
-  const value = document.getElementById('textarea1').value
+  const value = document.getElementById('textarea').value
   console.log(value);
-  $('#search-form')[0].reset();
-  let queryUrl ="https://developer.nps.gov/api/v1/parks?stateCode=" + value + "&api_key=a3wuSw2Td9o5F68gcq0f7e4Q8HzlBztPwFUaHzZ5"; 
+  $("#search-form")[0].reset();
+  let queryUrl ="https://developer.nps.gov/api/v1/parks?stateCode="+ value +"&api_key=a3wuSw2Td9o5F68gcq0f7e4Q8HzlBztPwFUaHzZ5&limit=10";
   $.ajax({
       url: queryUrl,
       method: "GET"
   }).then(function(response){
-      console.log(response)
-      console.log(response.data.fullName)
-      
-      
-      
+      let results = response.data;
+      console.log(results)
+          for (var i = 0; i < results.length; i++) {
+          var natParkDiv = $("<td>");
+          var hThree = $("<td>");
+          hThree.text(results[i].fullName);
+          var pOne = $("<td>");
+          pOne.text(results[i].description);
+          var pTwo = $("<td>");
+          pTwo.text(results[i].weatherInfo);
+          var pThree = $("<td>");
+          pThree.text(results[i].url);
+          natParkDiv.append(hThree);
+          natParkDiv.append(pOne);
+          natParkDiv.append(pTwo);
+          natParkDiv.append(pThree);
+          $("tbody").prepend(natParkDiv);
+       };
   })
-
 });
-
-
